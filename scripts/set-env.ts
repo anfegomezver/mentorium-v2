e78f51f6f110a9700f66948a5e5efef32034f233
Andres_Gomez_WPOSS <andresgomez@wposs.com>
2025-06-16 08:35:57 -0500

    Ajustes en la llave de producción

---
+++ scripts/set-env.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+
+const targetPath = '../src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: '${process.env['NG_APP_FIREBASE_API_KEY']}',
+    authDomain: '${process.env['NG_APP_AUTH_DOMAIN']}',
+    projectId: '${process.env['NG_APP_PROJECT_ID']}',
+    storageBucket: '${process.env['NG_APP_STORAGE_BUCKET']}',
+    messagingSenderId: '${process.env['NG_APP_MESSAGING_SENDER_ID']}',
+    appId: '${process.env['NG_APP_APP_ID']}'
+  }
+};`;
